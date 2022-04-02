2d49a4cc62a6c3f50e84144c41de7c7d160d5b22
Dylan <dylanljenkins@gmail.com>
2022-04-02 15:42:48 +1100

    Asteroid spawner

--- src/LD50.ts
+++ src/LD50.ts
@@ -1,13 +1,17 @@
 import {CollisionMatrix, Game, Log, LogLevel, Scene} from "lagom-engine";
 import {Asteroid, Earth, PhysicsEngine} from "./Physics";
+import {GameManager, GameManagerSystem} from "./Code/GameManager";
 
-enum Layers
+export enum Layers
 {
     Asteroid,
     Earth,
     Ship
 }
 
+export const GAME_WIDTH = 426;
+export const GAME_HEIGHT = 240;
+export const PLAYABLE_WIDTH = 350;
 
 const matrix = new CollisionMatrix();
 matrix.addCollision(Layers.Asteroid, Layers.Asteroid);
@@ -25,8 +29,10 @@ class MainScene extends Scene
 
         this.addEntity(new Earth("earth", 213, 120));
         this.addEntity(new Asteroid(10, 19));
+        this.addEntity(new GameManager("Game Manager"));
 
         this.addSystem(new PhysicsEngine());
+        this.addSystem(new GameManagerSystem());
     }
 }
 
@@ -34,7 +40,7 @@ export class LD50 extends Game
 {
     constructor()
     {
-        super({width: 426, height: 240, resolution: 3, backgroundColor: 0x0d2b45});
+        super({width: GAME_WIDTH, height: GAME_HEIGHT, resolution: 3, backgroundColor: 0x0d2b45});
 
         // TODO enable this before deploy
         // Log.logLevel = LogLevel.ERROR;
