c45ad1325cf19ddb0111a5772543e7ad3d79de64
Adrian Seddon <MasterAge@bitbucket.org>
2022-04-02 21:31:53 +1100

    Integrated rocket launching into typing

--- src/Code/Score.ts
+++ src/Code/Score.ts
@@ -1,7 +1,7 @@
 import {Component, Entity, TextDisp} from "lagom-engine";
 
 class Score extends Component {
-    remaining = 7_900_000;
+    remaining = 7_900_000_000;
     saved = 0;
 }
 
@@ -10,10 +10,10 @@ export class ScoreDisplay extends Entity {
         super.onAdded();
         const score = this.addComponent(new Score());
         this.addComponent(new TextDisp(0, 0, `Remaining: ${score.remaining.toLocaleString()}`, {
-            fontSize: 12
+            fontSize: 12, fill: "white"
         }));
         this.addComponent(new TextDisp(0, 15, `Saved: ${score.saved.toLocaleString()}`, {
-            fontSize: 12
+            fontSize: 12, fill: "white"
         }));
     }
 }
