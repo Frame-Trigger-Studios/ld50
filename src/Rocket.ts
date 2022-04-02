94b663519a715b8bf1f1679d936679b94a5f282b
Dylan Jenkins <dylanljenkins@gmail.com>
2022-04-02 23:08:11 +1100

    Rockets own their own force instead of asteroids

--- src/Rocket.ts
+++ src/Rocket.ts
@@ -5,38 +5,41 @@ import {
     Component,
     Entity,
     Game,
-    Log,
     MathUtil,
-    RenderCircle,
     Rigidbody,
     SimplePhysicsBody,
-    Sprite,
-    Timer
+    Sprite
 } from "lagom-engine";
-import {Force} from "./Physics";
+import {Asteroid, Force} from "./Physics";
 import {EARTH_X, EARTH_Y, Layers, RocketType} from "./LD50";
 import {OffScreenDestroyable} from "./Code/OffScreenDestroyer";
+import {DestroyMeNextFrame} from "./DestroyMeNextFrame";
 
 const SMALL_MISSILE_RADIUS = 30;
 const BIG_MISSILE_RADIUS = 75;
 const SMALL_PASSENGER_COUNT = 100;
 const BIG_PASSENGER_COUNT = 100;
 
-export class PassengerShip extends Component {
+export class PassengerShip extends Component
+{
 
-    constructor(public capacity: number) {
+    constructor(public capacity: number)
+    {
         super();
     }
 }
 
-export class Missile extends Component {
+export class Missile extends Component
+{
 
-    constructor(public explosionRadius: number) {
+    constructor(public explosionRadius: number)
+    {
         super();
     }
 }
 
-export class Rocket extends Entity {
+export class Rocket extends Entity
+{
 
     constructor(x: number, y: number, readonly speed: number, readonly rocketType: RocketType)
     {
@@ -47,19 +50,26 @@ export class Rocket extends Entity {
     {
         super.onAdded();
 
-        if (this.rocketType == RocketType.MISSILE) {
+        if (this.rocketType == RocketType.MISSILE)
+        {
             this.addComponent(new Missile(SMALL_MISSILE_RADIUS));
-        } else if (this.rocketType == RocketType.ICBM) {
+        }
+        else if (this.rocketType == RocketType.ICBM)
+        {
             this.addComponent(new Missile(BIG_MISSILE_RADIUS));
-        } else if (this.rocketType == RocketType.PASSENGER) {
+        }
+        else if (this.rocketType == RocketType.PASSENGER)
+        {
             this.addComponent(new PassengerShip(SMALL_PASSENGER_COUNT));
-        } else if (this.rocketType == RocketType.STARSHIP) {
+        }
+        else if (this.rocketType == RocketType.STARSHIP)
+        {
             this.addComponent(new PassengerShip(BIG_PASSENGER_COUNT));
         }
 
         const mousePos = this.scene.camera.viewToWorld(Game.mouse.getPosX(), Game.mouse.getPosY());
         const direction = MathUtil.pointDirection(EARTH_X, EARTH_Y,
-            mousePos.x, mousePos.y);
+                                                  mousePos.x, mousePos.y);
         const velocity = MathUtil.lengthDirXY(0.1, -direction);
 
         this.addComponent(new OffScreenDestroyable());
@@ -69,13 +79,48 @@ export class Rocket extends Entity {
         this.addComponent(new SimplePhysicsBody({angDrag: 0, linDrag: 0}));
         // TODO change the 3 to the rocketType
         const texture = this.getScene().game.getResource("rockets").texture(3, 0);
-        this.addComponent(new Sprite(texture, {xAnchor: 0.5, yAnchor: 0.5, rotation: -direction + MathUtil.degToRad(90)}));
+        this.addComponent(
+            new Sprite(texture, {xAnchor: 0.5, yAnchor: 0.5, rotation: -direction + MathUtil.degToRad(90)}));
 
-        const coll = this.addComponent(new CircleCollider(this.getScene().getGlobalSystem(CollisionSystem) as CollisionSystem, {
-            layer: Layers.Ship,
-            radius: 5,
-            xOff: 0,
-            yOff: 0
-        }));
+        const coll = this.addComponent(
+            new CircleCollider(this.getScene().getGlobalSystem(CollisionSystem) as CollisionSystem, {
+                layer: Layers.Ship,
+                radius: 5,
+                xOff: 0,
+                yOff: 0
+            }));
+
+        coll.onTriggerEnter.register((caller, {other, result}) => {
+            if (other.layer === Layers.Asteroid)
+            {
+                this.explode();
+            }
+        });
+    }
+
+    explode()
+    {
+        const missile = this.getComponent<Missile>(Missile);
+        if (!missile)
+        {
+            return;
+        }
+        const explosion = this.addComponent(
+            new CircleCollider(this.getScene().getGlobalSystem(CollisionSystem) as CollisionSystem, {
+                layer: Layers.Explosion,
+                radius: missile.explosionRadius,
+                xOff: 0,
+                yOff: 0
+            }));
+
+        explosion.onTriggerEnter.register((caller, {other, result}) => {
+            if (other.layer === Layers.Asteroid)
+            {
+                (other.getEntity() as Asteroid).pushFromCenter(this);
+            }
+        });
+
+        // Allow a frame to pass so that each asteroid can be collided with.
+        this.addComponent(new DestroyMeNextFrame());
     }
 }
