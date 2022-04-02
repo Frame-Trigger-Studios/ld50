df015a37698dacc93cf9cb05563059130e7fe480
Adrian Seddon <MasterAge@bitbucket.org>
2022-04-02 20:11:21 +1100

    Rewrote typeing system to fix a single bug

--- src/LD50.ts
+++ src/LD50.ts
@@ -61,7 +61,7 @@ class MainScene extends Scene
             this.addGlobalSystem(new DebugCollisionSystem(collSystem));
         }
 
-        this.addSystem(new TypingSystem());
+        this.addGlobalSystem(new TypingSystem());
 
         this.addEntity(new Earth("earth", 213, 120));
         this.addEntity(new GameManager("Game Manager"));
